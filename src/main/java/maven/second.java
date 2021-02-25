43f13d53bf740324792f58c12c506cd8ad4f6057
PriyankaNeelam <priyanka.netcool@gmail.com>
2021-02-24 22:59:55 -0700

    New Code

---
+++ src/main/java/maven/second.java
@@ -0,0 +1,26 @@
+package maven;
+import org.testng.annotations.*;
+public class second {
+		 @BeforeClass
+		 private void beforeClass() {
+			System.out.println("Before Class");
+			}
+		    @BeforeMethod
+		    private void beforeMethod() {
+				System.out.println("Before Method");
+			}
+		    @AfterClass
+		    private void afterClass() {
+				System.out.println("After Class");
+			}
+		    @AfterMethod
+		    private void afterMethod() {
+				System.out.println("After Method");
+			}
+		    @Test
+		    private void test1() {
+				System.out.println("test1");
+				System.out.println("test2");
+		}
+		}
+
